0b52d9eb61ec2d973202d6990294e8922e78ba31
Hooman <105157716+Hooman1994@users.noreply.github.com>
2024-07-18 12:53:02 +0330

    responsive fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./Layout";
 import "react-multi-carousel/lib/styles.css";
 import "./index.scss";
+import ResponsiveLayout from "./ResponsiveLayout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Layout />
+      <ResponsiveLayout />
     </BrowserRouter>
   </React.StrictMode>
 );
