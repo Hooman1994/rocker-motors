5dce322393a1780f4998d445329efced438c4d4a
Hooman <105157716+Hooman1994@users.noreply.github.com>
2024-10-18 17:31:39 +0330

    header desktop and mobile updated

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,13 +5,30 @@ import headerCar1 from "../assets/header-car1.svg";
 import headerCar3 from "../assets/header-car3.svg"; // Ensure you have the necessary CSS file
 
 export default function Header() {
+  const scrollToAboutUs = () => {
+    const aboutUsSection = document.getElementById("about-us");
+    if (aboutUsSection) {
+      aboutUsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const scrollToContactUs = () => {
+    const contactUsSection = document.getElementById("contact-us");
+    if (contactUsSection) {
+      contactUsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="header">
       <img style={{ left: "0" }} className="header-image" src={headerCar1} alt="header1" />
       <div className="header-text">
-        <span style={{ marginBottom: "20px", cursor: "pointer" }}>درباره ما</span>
+        <span onClick={scrollToAboutUs} style={{ marginBottom: "20px", cursor: "pointer" }}>
+          درباره ما
+        </span>
         <img style={{ cursor: "pointer" }} src={HeaderLogo} alt="logo" />
-        <span style={{ marginBottom: "20px", cursor: "pointer" }}>ارتباط با ما</span>
+        <span onClick={scrollToContactUs} style={{ marginBottom: "20px", cursor: "pointer" }}>
+          ارتباط با ما
+        </span>
       </div>
       <img style={{ right: "0" }} className="header-image" src={headerCar3} alt="header3" />
     </div>
--- src/Component/MobileHeader.jsx
+++ src/Component/MobileHeader.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Carousel from "./Carousel";
 import "../scss/header.scss";
 import HeaderLogo from "../assets/header-logo.svg";
-import headerCar1 from "../assets/header-car1.svg";
 import headerCar3 from "../assets/header-car3.svg"; // Ensure you have the necessary CSS file
 
 export default function MobileHeader() {
