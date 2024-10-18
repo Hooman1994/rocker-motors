2e6ae5d2b302c78d89a234af19546c2a5067cbf8
Hooman <105157716+Hooman1994@users.noreply.github.com>
2024-10-18 17:30:41 +0330

    carousel data added

---
+++ src/assets/data/CarouselData.js
@@ -0,0 +1,78 @@
+import Benz1 from "../data/cars/Benz (1).jpg";
+import Benz2 from "../data/cars/Benz (2).jpg";
+import Benz3 from "../data/cars/Benz (3).jpg";
+import Benz4 from "../data/cars/Benz (4).jpg";
+import Benz5 from "../data/cars/Benz (5).jpg";
+import BMW1 from "../data/cars/BMW (1).jpg";
+import BMW2 from "../data/cars/BMW (2).jpg";
+import BMW3 from "../data/cars/BMW (3).jpg";
+import BMW4 from "../data/cars/BMW (4).jpg";
+import BMW5 from "../data/cars/BMW (5).jpg";
+import Toyota1 from "../data/cars/Toyota (1).jpg";
+import Toyota2 from "../data/cars/Toyota (2).jpg";
+import Toyota3 from "../data/cars/Toyota (3).jpg";
+import Toyota4 from "../data/cars/Toyota (4).jpg";
+import Toyota5 from "../data/cars/Toyota (5).jpg";
+import Volvo1 from "../data/cars/Volvo (1).jpg";
+import Volvo2 from "../data/cars/Volvo (2).jpg";
+import Volvo3 from "../data/cars/Volvo (3).jpg";
+import Volvo4 from "../data/cars/Volvo (4).jpg";
+import Volvo5 from "../data/cars/Volvo (5).jpg";
+
+const CarouselData = [
+  {
+    defaultImage: Benz1,
+    title: "Benz E250",
+    info: "2015 - 70kM",
+    year: "2018",
+    kilometer: "70KM",
+    description: "an Amzing car with full option facilities.",
+    bodyCondition: "بدون رنگ",
+    status: 1,
+    price: 5750000000,
+    phone: "09125051070",
+    images: [Benz1, Benz2, Benz3, Benz4, Benz5],
+  },
+
+  {
+    defaultImage: BMW1,
+    title: "BMW",
+    info: "2015 - 70kM",
+    year: "2018",
+    kilometer: "70KM",
+    description: "an Amzing car with full option facilities.",
+    bodyCondition: "بدون رنگ",
+    status: 1,
+    price: 3750000000,
+    phone: "09125051070",
+    images: [BMW1, BMW2, BMW3, BMW4, BMW5],
+  },
+
+  {
+    defaultImage: Toyota1,
+    title: "Toyota",
+    info: "2015 - 70kM",
+    year: "2018",
+    kilometer: "70KM",
+    description: "an Amzing car with full option facilities.",
+    bodyCondition: "لکه رنگ",
+    status: 2,
+    price: 4250000000,
+    phone: "09125051070",
+    images: [Toyota1, Toyota2, Toyota3, Toyota4, Toyota5],
+  },
+  {
+    defaultImage: Volvo1,
+    title: "Volvo",
+    info: "2015 - 70kM",
+    year: "2018",
+    kilometer: "70KM",
+    description: "an Amzing car with full option facilities.",
+    bodyCondition: "با رنگ",
+    status: 3,
+    price: 2400000000,
+    phone: "09125051070",
+    images: [Volvo1, Volvo2, Volvo3, Volvo4, Volvo5],
+  },
+];
+export default CarouselData;
